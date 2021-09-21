7a9c8cfa7b6098c46b48de8b38c31bd2ec08363b
Rodrigo Vianna <roodrigo.vs123@hotmail.com>
2021-09-20 21:33:50 -0300

    add filter todo

--- script.js
+++ script.js
@@ -3,15 +3,37 @@ const todo = {
   ul: document.querySelector('.todo__list'),
   todos: [],
 
-  addEventListener() {
-    this.input.addEventListener('keypress', (e) => {
-      if (e.charCode === 13) {
-        e.preventDefault();
-        this.todos.push(this.input.value);
-        this.reload();
-        this.input.value = '';
-      }
+  init() {
+    this.addTodo();
+    this.removeTodo();
+    this.checkTodo();
+    filter.remainingItems();
+    filter.allFilter();
+  },
+
+  reload() {
+    this.clearTodo();
+    this.init();
+  },
+
+  addTodo() {
+    this.todos.forEach((todo) => {
+      const statusClass = todo.status === 'do' ? '' : 'done';
+      const iconClass =
+        todo.status === 'do' ? 'far fa-circle' : 'fas fa-check-circle';
+      const html = `   
+        <li class="todo__item ${statusClass}">   
+          <i class="check ${iconClass}"></i>
+          <p>${todo.text}</p>
+          <i class="remove fas fa-minus-circle"></i>  
+        </li>    
+      `;
+      this.ul.innerHTML += html;
     });
+  },
+
+  clearTodo() {
+    this.ul.innerHTML = '';
   },
 
   removeTodo() {
@@ -24,38 +46,121 @@ const todo = {
     });
   },
 
-  addTodo() {
-    this.todos.forEach((todo) => {
-      const html = `      
-        <i class="status far fa-circle"></i>
-        <p>${todo}</p>
-        <i class="remove fas fa-minus-circle"></i>      
-      `;
-      this.appendTodo(html);
+  checkTodo() {
+    const checkBtn = document.querySelectorAll('.todo__item');
+    checkBtn.forEach((btn, index) => {
+      btn.addEventListener('click', () => {
+        if (this.todos[index].status === 'do') {
+          this.todos[index].status = 'done';
+        } else {
+          this.todos[index].status = 'do';
+        }
+
+        this.reload();
+      });
     });
   },
 
-  appendTodo(html) {
-    const li = document.createElement('li');
-    li.classList.add('todo__item');
-    li.innerHTML = html;
-    this.ul.appendChild(li);
-  },
-
-  clearTodo() {
-    this.ul.innerHTML = '';
-  },
-
-  init() {
-    this.addEventListener();
-    this.removeTodo();
-  },
-
-  reload() {
-    this.clearTodo();
-    this.addTodo();
-    this.removeTodo();
+  addEventListener() {
+    this.input.addEventListener('keypress', (e) => {
+      if (e.charCode === 13) {
+        e.preventDefault();
+        if (this.input.value.length > 0) {
+          this.todos.push({
+            text: this.input.value,
+            status: 'do',
+          });
+          this.reload();
+          this.input.value = '';
+        } else {
+          alert('Você deve digitar algo!');
+        }
+      }
+    });
   },
 };
 
+const filter = {
+  allButton: document.querySelector('[data-filter="all"]'),
+  activeButton: document.querySelector('[data-filter="active"]'),
+  doneButton: document.querySelector('[data-filter="done"]'),
+  span: document.getElementById('itens'),
+
+  remainingItems() {
+    const items = document.querySelectorAll('li:not(.done)');
+    const doneItems = document.querySelectorAll('li.done');
+
+    if (filter.doneButton.classList.contains('active')) {
+      this.span.innerText = `${doneItems.length} itens finalizados`;
+    } else if (items.length === 0) {
+      this.span.innerText = 'Nenhum item ativo';
+    } else {
+      this.span.innerText = `${items.length} itens restantes`;
+    }
+  },
+
+  addFilterEvent() {
+    this.allButton.addEventListener('click', this.allFilter);
+    this.activeButton.addEventListener('click', this.activeFilter);
+    this.doneButton.addEventListener('click', this.doneFilter);
+  },
+
+  removeActive() {
+    this.allButton.classList.remove('active');
+    this.activeButton.classList.remove('active');
+    this.doneButton.classList.remove('active');
+  },
+
+  allFilter() {
+    filter.removeActive();
+    filter.allButton.classList.add('active');
+    filter.remainingItems();
+
+    const todos = document.querySelectorAll('.todo__item');
+
+    todos.forEach((todo) => {
+      todo.style.display = '';
+    });
+  },
+
+  activeFilter() {
+    filter.removeActive();
+    filter.activeButton.classList.add('active');
+    filter.remainingItems();
+
+    const todos = document.querySelectorAll('.todo__item');
+
+    todos.forEach((todo) => {
+      if (todo.classList.contains('done')) {
+        todo.style.display = 'none';
+      } else {
+        todo.style.display = '';
+      }
+    });
+  },
+
+  doneFilter() {
+    filter.removeActive();
+    filter.doneButton.classList.add('active');
+    filter.remainingItems();
+
+    const todos = document.querySelectorAll('.todo__item');
+
+    todos.forEach((todo) => {
+      if (todo.classList.contains('done')) {
+        todo.style.display = '';
+      } else {
+        todo.style.display = 'none';
+      }
+    });
+  },
+
+  init() {
+    this.addFilterEvent();
+    this.remainingItems();
+  },
+};
+
+todo.addEventListener();
 todo.init();
+filter.init();
